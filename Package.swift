7bdc4a0cf8398771a97bb429a40b961a53b22caf
CIBGlobal <155424494+CIBGlobal@users.noreply.github.com>
2025-07-18 14:09:25 +0800

    Add files via upload
    已集成上述功能, 并在本地做完测试

---
+++ Package.swift
@@ -0,0 +1,53 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "oneSDK",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "oneSDK",
+            targets: ["oneSDK"]),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/facebook/facebook-ios-sdk.git",
+            from: "18.0.0"),
+        .package(
+            url: "https://github.com/google/GoogleSignIn-iOS.git",
+            from: "8.0.0"),
+        .package(
+            url: "https://github.com/adjust/ios_sdk.git",
+            from: "5.4.0")
+    ],
+    targets: [
+        .binaryTarget(
+            name: "oneSDKBranch",
+            url: "https://github.com/CIBGlobal/onesdk-ios/raw/main/oneSDK.xcframework.zip",
+            checksum: "0a77d870462d79c42aade35c4b2e4290fc7dbc4407d7d11174fb3378edd598f8"
+        ),
+        .target(
+            name: "oneSDK",
+            dependencies: [
+                "oneSDKBranch",
+                .product(name: "FacebookAEM", package: "facebook-ios-sdk"),
+                .product(name: "FacebookBasics", package: "facebook-ios-sdk"),
+                .product(name: "FacebookCore", package: "facebook-ios-sdk"),
+                .product(name: "FacebookLogin", package: "facebook-ios-sdk"),
+                .product(name: "FacebookShare", package: "facebook-ios-sdk"),
+                .product(name: "FacebookGamingServices", package: "facebook-ios-sdk"),
+                .product(name: "AdjustGoogleOdm", package: "ios_sdk"),
+                .product(name: "AdjustSdk", package: "ios_sdk"),
+                .product(name: "AdjustWebBridge", package: "ios_sdk"),
+                .product(name: "GoogleSignIn", package: "GoogleSignIn-iOS"),
+                .product(name: "GoogleSignInSwift", package: "GoogleSignIn-iOS")
+            ],
+        ),
+        .testTarget(
+            name: "oneSDKTests",
+            dependencies: ["oneSDK"]
+        ),
+    ]
+)
