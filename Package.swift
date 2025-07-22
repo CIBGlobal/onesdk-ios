63b052414b5d977131865cd7174028c88d768aab
CIBGlobal <155424494+CIBGlobal@users.noreply.github.com>
2025-07-22 16:46:25 +0800

    修正Adjust在Unity或者Tuanjie引擎中无法导入问题

--- Package.swift
+++ Package.swift
@@ -6,32 +6,26 @@ import PackageDescription
 let package = Package(
     name: "oneSDK",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "oneSDK",
             targets: ["oneSDK"]),
     ],
     dependencies: [
-        .package(
-            url: "https://github.com/facebook/facebook-ios-sdk.git",
-            from: "14.1.0"),
-        .package(
-            url: "https://github.com/google/GoogleSignIn-iOS.git",
-            from: "8.0.0"),
-        .package(
-            url: "https://github.com/adjust/ios_sdk.git",
-            from: "5.4.0")
+        .package(url: "https://github.com/facebook/facebook-ios-sdk.git", from: "14.1.0"),
+        .package(url: "https://github.com/google/GoogleSignIn-iOS.git", from: "8.0.0"),
+        .package(url: "https://github.com/adjust/ios_sdk.git", from: "5.4.0")
     ],
     targets: [
         .binaryTarget(
             name: "oneSDKBranch",
             url: "https://github.com/CIBGlobal/onesdk-ios/raw/main/oneSDK.xcframework.zip",
-            checksum: "9f5044ef3051b0507141b0ca3601c67150213427af678bf1dcdbd030c42d1996"
+            checksum: "cfd703723d16d4be02653d6cfee7b191b5866a643421d357b8fdf753a2831878"
         ),
         .target(
             name: "oneSDK",
             dependencies: [
                 "oneSDKBranch",
+                // Facebook 依赖
                 .product(name: "FacebookAEM", package: "facebook-ios-sdk"),
                 .product(name: "FacebookBasics", package: "facebook-ios-sdk"),
                 .product(name: "FacebookCore", package: "facebook-ios-sdk"),
@@ -43,7 +37,7 @@ let package = Package(
                 .product(name: "AdjustWebBridge", package: "ios_sdk"),
                 .product(name: "GoogleSignIn", package: "GoogleSignIn-iOS"),
                 .product(name: "GoogleSignInSwift", package: "GoogleSignIn-iOS")
-            ],
+            ]
         ),
         .testTarget(
             name: "oneSDKTests",
--- README.md
+++ README.md
@@ -1,9 +1,12 @@
-# 本SDK已集成Facebook登录、Google登录、苹果登录、CIB ID登录、苹果支付、Adjust广告植入  
-Facebook版本14.1.0  
+# 本SDK已集成Facebook登录、Google登录、苹果登录、CIB ID登录、Adjust广告植入  
+Facebook版本18.0.0  
 Google版本8.0.0  
 Adjust版本5.4.0  
 
 未经授权, 禁止使用  
+
+## Version 1.0.5  
+修正Adjust在Unity或者Tuanjie引擎中无法导入问题  
 
 ## Version 1.0.4  
 faceoobk版本降级为14.1.0, 适配旧一点的ios项目  
